53d057c5283ffbf146a997fa5d5be2f340d8375f
ghebremichael, filmon <filmo2020gh@gmial.com>
2023-04-30 19:04:05 +0200

    Add start functionality to display quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 
 const activateButton = document.getElementById("Start");
-
+const quizQuestion = document.getElementById("question")
+const quizChoices = document.getElementById("choices")
 
 activateButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
     console.log("started")
     activateButton.classList.add("hide");
+    quizQuestion.classList.remove("hide");
+    quizChoices.classList.remove("hide");
    
   }
 
