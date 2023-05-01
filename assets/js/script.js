73b3ee4cf113032943b92f4ab7551ca37c077d94
ghebremichael, filmon <filmo2020gh@gmial.com>
2023-05-01 13:36:31 +0200

    Add clearQuizState function for resetting quiz state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
 const activateButton = document.getElementById("Start");
+const  nextButton = document.getElementById("Next");
 const quizQuestion = document.getElementById("question");
 const quizChoices = document.getElementById("choices");
 const questionDisplay = document.getElementById("question");
@@ -23,8 +24,13 @@ function startQuiz() {
   }
 
   function NextQuestion() {
+    clearQuizState()
     showQuestion(randomizedQuestions[currentQuestion])
+   
   }
+
+
+
   function showQuestion(question) {
     questionDisplay.innerText = question.question;
     question.choices.forEach(choice => {
@@ -39,17 +45,42 @@ function startQuiz() {
     });
   }  
 
+  function clearQuizState() {
+    nextButton.classList.add("hide")
+    while ( answerChoices.firstChild) {
+      answerChoices.removeChild( answerChoices.firstChild)
+    }
+  }
+
+
+
 
   function selectAnswer(e) {
   
   }
 
+
+
+
+  
+
   const questions = [
     {
       question: 'Which of the following is not a data type in JavaScript?',
       choices: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
+        { text: 'Boolean', correct: false },
+        { text: 'Number', correct: false },
       ]
     },
-]
+  ];
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
