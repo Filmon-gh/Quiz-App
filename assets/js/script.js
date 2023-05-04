61c15dd1673669d6819486c3ca0696e2fbf6c902
ghebremichael, filmon <filmo2020gh@gmial.com>
2023-05-04 06:59:49 +0200

    Disable  choice buttons when user selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,16 @@ const answerChoices = document.getElementById("choices");
 const container = document.querySelector(".container");
 const questionNumberElement = document.getElementById("question-number");
 const timerElement = document.getElementById("timer");
-const timerContainer= document.querySelector(".timer");
 const resultBox =document.querySelector(".Result_box");
 const containerBox =document.querySelector(".container");
 const scoreResult =document.querySelector(".score_text");
 const restartQuiz= document.querySelector(".restart");
 
 
-let randomizedQuestions, currentQuestion, totalQuestions, countdown;
+let randomizedQuestions = null;
+let currentQuestion = null;
+let totalQuestions = null;
+let countdown = null;
 let timeLeft =20;
 let score=0;
 
@@ -26,38 +28,53 @@ nextQuestionButton.addEventListener('click', () => {
   clearInterval(countdown);
   startTimer(timeLeft);
   NextQuestion(); 
-})
+});
 
 
-// Function to show the next question and update the question number. It clears the current quiz state first.
+/**
+Function to show the next question and update the question number.
+It clears the current quiz state first.
+*/
 function NextQuestion() {
-  clearQuizState()
-  showQuestion(randomizedQuestions[currentQuestion])
+  clearQuizState();
+  showQuestion(randomizedQuestions[currentQuestion]);
   updateQuestionNumber();
-
+  startTimer(20);
 }
 
-// Function to start the quiz by setting up the initial quiz state, randomizing the questions, and calling the NextQuestion function to display the first question
+/**
+Function to start the quiz by setting up the initial quiz state,
+randomizing the questions, and calling the NextQuestion function
+to display the first question
+*/
 function startQuiz() {
-    console.log("started")
+    console.log("started");
     activateButton.classList.add("hide");
-    resultBox.classList.add("hide")
+    resultBox.classList.add("hide");
     quizQuestion.classList.remove("hide");
     quizChoices.classList.remove("hide"); 
     randomizedQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestion = 0
+    currentQuestion = 0;
     totalQuestions = randomizedQuestions.length;
     questionNumberElement.classList.remove("hide");// Show the question number element by removing the "hide" class.
     updateQuestionNumber();
-    NextQuestion() 
+    NextQuestion();
     startTimer(20);
    
   }
-  // Updates the displayed question number based on the current question and the total number of questions
-  function updateQuestionNumber() {
+/**
+   Updates the displayed question number based on the current question and the total
+    number of questions
+*/
+function updateQuestionNumber() {
     questionNumberElement.innerText = `${currentQuestion + 1}/${totalQuestions}`;
   }
-  // Function to display the current question and its answer choices. It creates a button for each answer choice and adds a click event listener to each button to call the selectAnswer function.
+  
+  /**
+Function to display the current question and its answer choices.
+It creates a button for each answer choice and adds a click event listener
+to each button to call the selectAnswer function.
+*/
   function showQuestion(question) {
     questionDisplay.innerText = question.question;
     question.choices.forEach(choice => {
@@ -73,64 +90,68 @@ function startQuiz() {
       
     });
   }  
- // Function to clear the current quiz state by removing the status class from the container, hiding the nextQuestionButton, and removing all answer choices.
+
+ /**
+Function to clear the current quiz state by removing the status class from the container,
+hiding the nextQuestionButton, and removing all answer choices.
+*/
   function clearQuizState() {
-    removeStatusClass(document.querySelector('.container'))
-    nextQuestionButton.classList.add("hide")
+    removeStatusClass(document.querySelector('.container'));
+    nextQuestionButton.classList.add("hide");
     while ( answerChoices.firstChild) {
-      answerChoices.removeChild( answerChoices.firstChild)
+      answerChoices.removeChild( answerChoices.firstChild);
     }
   }
 
+/**
+Updates the status of the container and each answer button based on the correctness of
+ the answer selected by the user.
+*/
 
-
-// Updates the status of the container and each answer button based on the correctness of the answer selected by the user
-
-  function selectAnswer(e) {
-    /*clearInterval(countdown);
-     startTimer(timeLeft); */
-    const chosenButton = e.target
-    const isCorrect  = chosenButton.dataset.correct
-    updateElementStatus(container, isCorrect )
+function selectAnswer(e) {
+    Array.from(document.getElementsByClassName('choice')).forEach(e => {e.disabled = true})
+    const chosenButton = e.target;
+    const isCorrect  = chosenButton.dataset.correct;
+    updateElementStatus(container, isCorrect );
     Array.from( answerChoices.children).forEach(button =>{
-      updateElementStatus(button, button.dataset.correct)
-    })
+      updateElementStatus(button, button.dataset.correct);
+    });
 
     if (isCorrect === 'true') {
-      score++
-      console.log(score)
+      score++ ;
     }
     if (randomizedQuestions.length > currentQuestion + 1) {
-      nextQuestionButton.classList.remove("hide")
+      nextQuestionButton.classList.remove("hide");
     } else {
-    
-     /* activateButton .innerText = "Restart"
-      activateButton.classList.remove("hide") */
       showresultBox();
     
     }
   }
 
   // Updates the status of an element based on whether the selected answer is correct or not
-  function updateElementStatus(myElement, isCorrect ) {
-    removeStatusClass(myElement)
+function updateElementStatus(myElement, isCorrect ) {
+    removeStatusClass(myElement);
     if (isCorrect ) {
-      myElement.classList.add("correct")
+      myElement.classList.add("correct");
     } else {
-      myElement.classList.add("wrong")
+      myElement.classList.add("wrong");
     }
   }
   
   // Removes the status class from an element
   function removeStatusClass(myElement) {
-    myElement.classList.remove("correct")
-    myElement.classList.remove("wrong")
+    myElement.classList.remove("correct");
+    myElement.classList.remove("wrong");
   }
   
-
-  // Function to start the countdown timer for the quiz, decrementing the time by one second until it reaches zero or the next question is shown. It also adds a leading zero to the timer element when the time is less than 10
+  /**
+  Starts the countdown timer for the quiz.
+  It decrements the time by one second until it reaches zero or the next question is shown.
+  It also adds a leading zero to the timer element when the time is less than 10.
+*/
   function startTimer(time) {
-    countdown = setInterval(timer, 1000)
+    clearInterval(countdown);
+    countdown = setInterval(timer, 1000);
     function timer () {
       timerElement.textContent = time;
       time--;
@@ -143,7 +164,7 @@ function startQuiz() {
         timerElement.textContent = "00";
         NextQuestion(); 
       clearInterval(countdown);
-      startTimer(20);
+      
       }
       /*else if(time === -1) { 
             NextQuestion(); 
@@ -154,14 +175,12 @@ function startQuiz() {
   }
 }
 
-
-
 // This function hides the quiz question, choices, and container boxes and displays the result box
   function showresultBox(){
     quizQuestion.classList.add("hide");
-    quizChoices.classList.add("hide")
-    containerBox.classList.add("hide")
-    resultBox.classList.remove("hide")
+    quizChoices.classList.add("hide");
+    containerBox.classList.add("hide");
+    resultBox.classList.remove("hide");
     if (score > 5) {
       let scoreElement = "<span>congrats!, you got <p> "+ score + "</p> out of<p>" + randomizedQuestions.length+ "</p> </span>"
       scoreResult.innerHTML=scoreElement ;
@@ -176,19 +195,12 @@ function startQuiz() {
       let scoreElement = "<span>and sorry, you got only<p> "+ score + "</p> out of<p>" + randomizedQuestions.length+ "</p> </span>"
       scoreResult.innerHTML=scoreElement ;
     }
-    
   }
-
   // Add event listener to restartQuiz button to reload the page on click
 restartQuiz.onclick = () => {
   window.location.reload();
   }
-  
-  
-
-
  // Array of objects containing quiz questions and choices for HTML, CSS, and JavaScript topics.
-
   const questions = [
     {
       question: 'What does HTML stand for ?',
@@ -204,7 +216,7 @@ restartQuiz.onclick = () => {
       choices: [
         { text: 'A) To add interactivity to a website', incorrect: false },
         { text: 'B) To create dynamic web pages', incorrect: false },
-        { text: 'C) To style and visually enhance the layout of a website', correct: true },
+        { text: 'C) To improve website layout appearance and design.', correct: true },
         { text: 'D) To handle the back-end functionality of a website', incorrect: false }
         ]
     },
@@ -222,7 +234,7 @@ restartQuiz.onclick = () => {
       choices: [
         { text: 'A) A model for creating 3D shapes in CSS', incorrect: false },
         { text: 'B) A model for organizing and displaying content on a web page', incorrect: false },
-        { text: 'C) A model for calculating the size and position of elements on a web page', correct: true },
+        { text: 'C) A layout model for web page elements', correct: true },
         { text: 'D) A model for creating rounded corners on boxes in CSS', incorrect: false }
         ]
     },
@@ -232,14 +244,14 @@ restartQuiz.onclick = () => {
         { text: 'A) To define the main content of the web page', incorrect: false },
         { text: 'B) To define the footer of the web page', incorrect: false },
         { text: 'C) To define the header of the web page', incorrect: false },
-        { text: 'D) To include metadata, such as the page title and links to external resources', correct: true }
+        { text: 'D) Add metadata for page title and external resource links', correct: true }
       ]
     },
     {
       question: 'What is the difference between == and === in JavaScript?',
       choices: [
-        { text: 'A) == compares the values of two variables while === compares both the value and the type of the variables', correct: true },
-        { text: 'B) == compares the types of two variables while === compares the values of the variables', incorrect: false },
+        { text: 'A) == compares values of two variables, whereas === compares both value and type.', correct: true },
+        { text: 'B) "== compares types, while === compares types and values.', incorrect: false },
         { text: 'C) == and === are the same thing', incorrect: false },
         { text: 'D) == and === are not used in JavaScript', incorrect: false }
         ]
@@ -255,14 +267,3 @@ restartQuiz.onclick = () => {
     },
 
   ];
-  
-  
-  
-
-  
-  
-  
-  
-  
-  
-  
